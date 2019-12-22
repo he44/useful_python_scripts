b41e78f83bd50900391274f340dc08a9337160d4
he44 <he44@illinois.edu>
2019-12-21 22:36:20 -0600

    Create tuner_test.py

---
+++ keras_tuner/tuner_test.py
@@ -0,0 +1,7 @@
+#  Official documentation: https://keras-team.github.io/keras-tuner/
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from kerastuner.tuners import RandomSearch
+
+
